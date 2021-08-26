429446afc598622735ee66703d5b0e19d14b88ca
Rana <ranabiswas02@gmail.com>
2021-08-26 17:44:07 +0600

    Initial commit

---
+++ Admin/connection.php
@@ -0,0 +1,4 @@
+<?php
+$db=mysqli_connect('localhost','rana','rana1996','practice');
+            				
+?>
---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+$db=mysqli_connect('localhost','rana','rana1996','practice');
+
+
+					 /***Font page Login */
+
+   
+                    
+									
+								
+				
+?>
